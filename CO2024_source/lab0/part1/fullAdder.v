e1ed347ea305da57075792fafde690fc3943fdcb
KexinChen0430 <1045921362@qq.com>
2025-05-13 20:14:58 +0800

    Add files via upload

---
+++ CO2024_source/lab0/part1/fullAdder.v
@@ -0,0 +1,9 @@
+module fullAdder(input cin, input a, input b, output s, output cout);
+    // FullAdder compute addition of input cin, a and b,
+    // then output result to s and carry bit to cout.
+
+    // TODO: implement your fullAdder design here
+    // For testbench verifying, do not modify input and output pin
+
+endmodule
+
---
+++ CO2024_source/lab0/part2/alu.v
@@ -0,0 +1,15 @@
+module alu(
+    input [3:0] a,
+    input [3:0] b,
+    input [2:0] s,
+    output reg [3:0] y
+); 
+    // alu has two input operand a and b.
+    // It executes different operation over input a and b based on input s
+    // then generate result to output y
+    
+    // TODO: implement your 4bits ALU design here and using your own fulladder module in this module
+    // For testbench verifying, do not modify input and output pin
+    
+endmodule
+
