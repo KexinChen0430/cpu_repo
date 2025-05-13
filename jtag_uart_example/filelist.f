69a0652dae454f8bfd55b5e4bcf2875bd825f3bf
KexinChen0430 <1045921362@qq.com>
2025-05-13 20:25:31 +0800

    Add files via upload

---
+++ jtag_uart_example/filelist.f
@@ -0,0 +1,13 @@
++incdir+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/simulation/submodules
++incdir+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart
++incdir+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/simulation
++incdir+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/synthesis/submodules
++incdir+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/synthesis
++incdir+./rtl
+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/jtag_uart_inst.v
+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/synthesis/submodules/jtag_uart_jtag_uart_0.v
+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/synthesis/jtag_uart.v
+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/jtag_uart_bb.v
+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/simulation/submodules/jtag_uart_jtag_uart_0.v
+./quartus_max10_deca/intel_models/jtag_uart/jtag_uart/simulation/jtag_uart.v
+./rtl/top.v
